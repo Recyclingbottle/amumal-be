4d01dc0ea5244aab24460b25fa5e21ee0bf83c73
Recyclingbottle <hoholeo382@gmail.com>
2024-04-19 14:21:55 +0900

    240419 게시글 관련 라우트 설정

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ require("dotenv").config(); // 환경변수 로드를 위해 dotenv 사용
 const jwtMiddleware = require("./middleware/jwtMiddleware"); // JWT 인증 미들웨어
 const userRoutes = require("./routes/userRoutes"); // 사용자 관련 라우트
 const uploadRoutes = require("./routes/uploadRoutes"); // 파일 업로드 관련 라우트
+const postRoutes = require("./routes/postRoutes"); // 게시글 라우트 불러오기
 
 const app = express();
 app.use(bodyParser.json()); // JSON 요청 본문 처리
 
 // 기본 경로
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("아무 말 대잔치 API 서버");
 });
 
 // 사용자 관련 라우트 설정
@@ -19,6 +20,9 @@ app.use("/users", userRoutes);
 
 // 파일 업로드 라우트 설정
 app.use("/upload", uploadRoutes);
+
+// 게시글 관련 라우트 설정
+app.use("/posts", postRoutes);
 
 // JWT 미들웨어를 사용하여 보호된 경로 설정
 app.get("/protected-route", jwtMiddleware.authenticateToken, (req, res) => {
